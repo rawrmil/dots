94930216a46f9a03763ad4a40165876f840ab052
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-04 10:34:46 +0000

    Auto-sync Sun May  4 10:34:46 AM UTC 2025

--- scripts/termux/t-debian-dwm.sh
+++ scripts/termux/t-debian-dwm.sh
@@ -2,4 +2,4 @@
 
 termux-x11 :0 &
 
-proot-distro login debian --shared-tmp -- bash -c "export DISPLAY=:0; dwm" &
+proot-distro login debian --shared-tmp -- bash -c "bash ~/.xinitrc; export DISPLAY=:0; dwm" &
