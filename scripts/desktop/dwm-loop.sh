8436e43a364253f3bd0f519478b745b6eb38292b
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-04 11:00:34 +0000

    Auto-sync Sun May  4 11:00:33 AM UTC 2025

--- scripts/desktop/dwm-loop.sh
+++ scripts/desktop/dwm-loop.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-~/dots/scripts/desktop/dwm-statusbar.sh &
 [ -z "$DISPLAY" ] && export DISPLAY=:0
+bash ~/dots/scripts/desktop/dwm-statusbar.sh &
 while true; do
 	echo "$DISPLAY"
 	dwm
-	echo "dwm exited. Restarting..."
 done
