4ebd6d8f1e0be94b97afb9d1b6416d0f8347d74a
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-04 11:20:45 +0000

    Auto-sync Sun May  4 11:20:45 AM UTC 2025

--- scripts/desktop/dwm-loop.sh
+++ scripts/desktop/dwm-loop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 [ -z "$DISPLAY" ] && export DISPLAY=:0
+xrdb -merge ~/.Xresources
+setxkbmap -layout us,ru -option grp:alt_shift_toggle
 bash ~/dots/scripts/desktop/dwm-statusbar.sh &
 while true; do
 	dwm
