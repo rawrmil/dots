c7f77cfc85b81c952fa34d049370bf4bb07dba7e
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-05 13:44:45 +0300

    Auto-sync Mon May  5 13:44:45 MSK 2025

--- scripts/termux/t-dwm-debian.sh
+++ scripts/termux/t-dwm-debian.sh
@@ -3,4 +3,4 @@
 pkill dwm-loop
 pkill app_process
 termux-x11 :0 &
-proot-distro login debian --shared-tmp -- bash -c "sudo bash ~/.xinitrc" &
+proot-distro login debian --shared-tmp --isolated -- bash -c "sudo bash ~/.xinitrc" &
