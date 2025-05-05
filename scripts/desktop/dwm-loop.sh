50201803a8c00ca6733776b0fd50f0f5d04be0c5
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-05 07:46:03 +0000

    Auto-sync Mon May  5 07:46:03 AM UTC 2025

--- scripts/desktop/dwm-loop.sh
+++ scripts/desktop/dwm-loop.sh
@@ -2,9 +2,9 @@
 set -x
 [ -z "$DISPLAY" ] && export DISPLAY=:0
 [ -z "$XDG_RUNTIME_DIR" ] && export XDG_RUNTIME_DIR=/run/user/$(id -u)
-xrdb -merge ~/.Xresources
-setxkbmap -layout us,ru -option grp:alt_shift_toggle
 bash ~/dots/scripts/desktop/dwm-statusbar.sh &
 while true; do
+	xrdb -merge ~/.Xresources
+	setxkbmap -layout us,ru -option grp:alt_shift_toggle
 	dwm
 done
