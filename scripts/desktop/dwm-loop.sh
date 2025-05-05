a806250449062f7bc30db9913ad162643001e942
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-05 06:09:22 +0000

    Auto-sync Mon May  5 06:09:22 AM UTC 2025

--- scripts/desktop/dwm-loop.sh
+++ scripts/desktop/dwm-loop.sh
@@ -2,6 +2,7 @@
 [ -z "$DISPLAY" ] && export DISPLAY=:0
 bash ~/dots/scripts/desktop/dwm-statusbar.sh &
 while true; do
+	echo "who: $(whoami)"
 	xrdb -merge ~/.Xresources
 	setxkbmap -layout us,ru -option grp:alt_shift_toggle
 	dwm
