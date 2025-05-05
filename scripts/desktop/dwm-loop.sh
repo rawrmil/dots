25150963e662bbdae6b3ed1d6beacd60c05b661d
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-05 07:40:43 +0000

    Auto-sync Mon May  5 07:40:43 AM UTC 2025

--- scripts/desktop/dwm-loop.sh
+++ scripts/desktop/dwm-loop.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+set -x
 [ -z "$DISPLAY" ] && export DISPLAY=:0
 [ -z "$XDG_RUNTIME_DIR" ] && export XDG_RUNTIME_DIR=/run/user/$(id -u)
 xrdb -merge ~/.Xresources
 setxkbmap -layout us,ru -option grp:alt_shift_toggle
 bash ~/dots/scripts/desktop/dwm-statusbar.sh &
 while true; do
-	echo "$XDG_RUNTIME_DIR"
-	sleep 1
 	dwm
 done
