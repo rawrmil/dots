216359e67da01add39d9f041a6836dd59c02e963
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-07 01:10:57 +0300

    Auto-sync Wed May  7 01:10:57 AM MSK 2025

--- scripts/desktop/dwm-loop.sh
+++ scripts/desktop/dwm-loop.sh
@@ -3,10 +3,9 @@ set -x
 [ -z "$DISPLAY" ] && export DISPLAY=:0
 [ -z "$XDG_RUNTIME_DIR" ] && export XDG_RUNTIME_DIR=/tmp
 bash ~/dots/scripts/desktop/dwm-statusbar.sh &
-# Picom restarts itself after touching the config
-picom -b --config ~/dots/conf/desktop/picom.conf
 while true; do
 	xrdb -merge ~/.Xresources
 	setxkbmap -layout us,ru -option grp:alt_shift_toggle
+	picom -b --config ~/dots/conf/desktop/picom.conf
 	dwm
 done
