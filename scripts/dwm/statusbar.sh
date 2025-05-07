6342c0632d929a34785844c3ce4630dd31f24ef8
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-07 12:19:12 +0300

    Auto-sync Wed May  7 12:19:12 PM MSK 2025

--- scripts/dwm/statusbar.sh
+++ scripts/dwm/statusbar.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 while true; do
-	# Battery
 	LINE=""
+	# Weather
+	curl wttr.in/moscow/?format="%t, %p, %w"
+	# Battery
 	BAT=$(acpi -b | grep -oP '[0-9]+(?=%)' | awk '{print "BAT:"$1"%"}' | head -1)
 	[ ! -z $BAT ] && LINE="$LINE$BAT "
 	# Some cool text
-	LINE="$LINE, Help me"
+	LINE="$LINE | Help me"
 	# Date+time
-	LINE="$LINE $(date)"
+	LINE="$LINE | $(date '+%Y-%m-%d %H:%M:%S')"
 	xsetroot -name "$LINE"
 	sleep 1
 done
