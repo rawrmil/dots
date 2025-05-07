4efc56e3b2155fd6cc9eb0f449fad3073fdd5d6a
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-07 12:50:32 +0300

    Auto-sync Wed May  7 12:50:32 PM MSK 2025

--- scripts/dwm/statusbar.sh
+++ scripts/dwm/statusbar.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-WEATHER="Pending..."
-(
-while true; do
-	WEATHER=$(curl "https://wttr.in/?format=%t,%p,%w")
-	sleep 30
-done
-) &
-
-BATTERY="Pending..."
-(
-while true; do
-	BATTERY=$(acpi -b | grep -oP '[0-9]+(?=%)' | awk '{print "POWER: "$1"%"}' | head -1)
-	[ ! -z $BATTERY ] && BATTERY="POWER: INFINITE"
-	sleep 10
-done
-) &
+#WEATHER="Pending..."
+#(
+#while true; do
+#	WEATHER=$(curl "https://wttr.in/?format=%t,%p,%w")
+#	sleep 30
+#done
+#) &
+#
+#BATTERY="Pending..."
+#(
+#while true; do
+#	BATTERY=$(acpi -b | grep -oP '[0-9]+(?=%)' | awk '{print "POWER: "$1"%"}' | head -1)
+#	[ ! -z $BATTERY ] && BATTERY="POWER: INFINITE"
+#	sleep 10
+#done
+#) &
 
 DATETIME="Pending..."
 (
@@ -29,7 +29,8 @@ done
 
 while true; do
 	LINE=""
-	LINE="$WEATHER | $BATTERY | HELP ME | $DATETIME"
+	#LINE="$WEATHER | $BATTERY | HELP ME | $DATETIME"
+	LINE="->$DATETIME<-"
 	xsetroot -name "$LINE"
 	sleep 1
 done
