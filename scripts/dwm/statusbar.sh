2d066cf6576f3b4ae6c796561bd1db3ee00ba3e3
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-07 13:03:02 +0300

    Auto-sync Wed May  7 01:03:02 PM MSK 2025

--- scripts/dwm/statusbar.sh
+++ scripts/dwm/statusbar.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
-#WEATHER="Pending..."
-#(
-#while true; do
-#	WEATHER=$(curl "https://wttr.in/?format=%t,%p,%w")
-#	sleep 30
-#done
-#) &
-#
-#BATTERY="Pending..."
-#(
-#while true; do
-#	BATTERY=$(acpi -b | grep -oP '[0-9]+(?=%)' | awk '{print "POWER: "$1"%"}' | head -1)
-#	[ ! -z $BATTERY ] && BATTERY="POWER: INFINITE"
-#	sleep 10
-#done
-#) &
+WEATHER="Pending..."
+(
+while true; do
+	WEATHER=$(curl "https://wttr.in/?format=%t,%p,%w")
+	sleep 30
+done
+) &
+
+BATTERY="Pending..."
+(
+while true; do
+	BATTERY=$(acpi -b | grep -oP '[0-9]+(?=%)' | awk '{print "POWER: "$1"%"}' | head -1)
+	[ ! -z $BATTERY ] && BATTERY="POWER: INFINITE"
+	sleep 10
+done
+) &
 
 DATETIME="Pending..."
 while true; do
-	DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
-	echo "$DATETIME"
+	echo $(date '+%Y-%m-%d %H:%M:%S')
 	sleep 1
 done &
 
 # M A I N L O O P
 
 while true; do
-	LINE=""
-	#LINE="$WEATHER | $BATTERY | HELP ME | $DATETIME"
-	LINE="->$DATETIME<-"
+	LINE="$WEATHER | $BATTERY | HELP ME | $DATETIME"
 	xsetroot -name "$LINE"
 	sleep 1
 done
