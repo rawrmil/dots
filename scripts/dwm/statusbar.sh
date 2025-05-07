3577eea3e7a0d959c214d143c8d481494be6fec1
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-07 12:44:27 +0300

    Auto-sync Wed May  7 12:44:27 PM MSK 2025

--- scripts/dwm/statusbar.sh
+++ scripts/dwm/statusbar.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
+
+WEATHER=
+(
+while true; do
+	WEATHER=$(curl "https://wttr.in/?format=%t,%p,%w")
+	sleep 30
+done
+) &
+
+BATTERYTERY=
+(
+while true; do
+	BATTERYTERY=$(acpi -b | grep -oP '[0-9]+(?=%)' | awk '{print "POWER: "$1"%"}' | head -1)
+	[ ! -z $BATTERY ] && BATTERY="POWER: INFINITE"
+	sleep 1
+done
+) &
+
+DATETIME=
+(
+while true; do
+	DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
+	sleep 1
+done
+) &
+
+# M A I N L O O P
+
 while true; do
 	LINE=""
-	# Weather
-	#WEATHER=$(curl "https://wttr.in/?format=%t,%p,%w")
-	#LINE="$LINE | $WEATHER"
-	# Battery
-	BAT=$(acpi -b | grep -oP '[0-9]+(?=%)' | awk '{print "BAT:"$1"%"}' | head -1)
-	[ ! -z $BAT ] && LINE="$LINE | $BAT"
-	# Some cool text
-	LINE="$LINE | Help me"
-	# Date+time
-	LINE="$LINE | $(date '+%Y-%m-%d %H:%M:%S')"
+	LINE="$WEATHER | $BATTERY | HELP ME | $DATETIME"
 	xsetroot -name "$LINE"
 	sleep 1
 done
