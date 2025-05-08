1025929ac00d8f854f6c706ec9739c91ee56b8ba
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-08 23:20:16 +0300

    Auto-sync Thu May  8 11:20:16 PM MSK 2025

--- scripts/dwm/rh-dwm-loop.sh
+++ scripts/dwm/rh-dwm-loop.sh
@@ -4,6 +4,7 @@ set -x
 [ -z "$XDG_RUNTIME_DIR" ] && export XDG_RUNTIME_DIR=/tmp
 while true; do
 	pkill -x rh-dwm-bar-run.sh
+	pkill -x rh-dwm-bar-update.bin
 	exec ~/dots/scripts/dwm/rh-dwm-bar-run.sh &
 	xrdb -merge ~/.Xresources
 	setxkbmap -layout us,ru -option grp:alt_shift_toggle
