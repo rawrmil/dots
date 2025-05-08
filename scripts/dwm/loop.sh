f7f0b3244d599f7c550c31af715ece5d05004a55
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-08 10:58:36 +0300

    Fvcking git broken again ahhh

--- scripts/dwm/loop.sh
+++ scripts/dwm/loop.sh
@@ -3,7 +3,7 @@ set -x
 [ -z "$DISPLAY" ] && export DISPLAY=:0
 [ -z "$XDG_RUNTIME_DIR" ] && export XDG_RUNTIME_DIR=/tmp
 while true; do
-	pkill -f ~/dots/scripts/dwm/statusbar.sh
+	pkill ~/dots/scripts/dwm/statusbar.sh
 	bash ~/dots/scripts/dwm/statusbar.sh &
 	xrdb -merge ~/.Xresources
 	setxkbmap -layout us,ru -option grp:alt_shift_toggle
--- scripts/dwm/statusbar.sh
+++ scripts/dwm/statusbar.sh
@@ -1,33 +1,6 @@
 #!/bin/bash
-exec ~/dots/scripts/dwm/test
-exit 0
-WEATHER="Pending..."
-(
-while true; do
-	WEATHER=$(curl "https://wttr.in/?format=%t,%p,%w")
-	sleep 30
-done
-) &
-
-BATTERY="Pending..."
-(
-while true; do
-	BATTERY=$(acpi -b | grep -oP '[0-9]+(?=%)' | awk '{print "POWER: "$1"%"}' | head -1)
-	[ ! -z $BATTERY ] && BATTERY="POWER: INFINITE"
-	sleep 10
-done
-) &
-
-DATETIME="Pending..."
-while true; do
-	echo $(date '+%Y-%m-%d %H:%M:%S')
-	sleep 1
-done &
-
-# M A I N L O O P
-
-while true; do
-	LINE="$WEATHER | $BATTERY | HELP ME | $DATETIME"
-	xsetroot -name "$LINE"
-	sleep 1
-done
+set -x
+set -e
+cd ~/dots/scripts/dwm/
+gcc -pthread -o statusbar statusbar.c -lX11
+exec ~/dots/scripts/dwm/statusbar
