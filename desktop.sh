65c970f513b369cd9248a5670d49eb77f0bcb2f5
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-23 19:54:35 +0300

    Auto-sync Fri May 23 07:54:35 PM MSK 2025

--- desktop.sh
+++ desktop.sh
@@ -4,37 +4,17 @@
 set -e
 set -x
 
-#echo "[desktop.sh] cd to ~/suckless..."
-#mkdir -p $HOME/suckless
-#cd $HOME/suckless
+cd $(realpath $0)
 
-#echo "[desktop.sh] Clone the suckless..."
-#[ ! -d ./dwm ] && git clone https://git.suckless.org/dwm
-#[ ! -d ./dmenu ] && git clone https://git.suckless.org/dmenu
-#[ ! -d ./st ] && git clone https://git.suckless.org/st
-#
-#echo "[desktop.sh] Symlink the config.h..."
-#if [ ! -L ./dwm/config.h ]; then
-#	[ -f ./dwm/config.h ] && rm ./dwm/config.h
-#	ln -s $HOME/dots/conf/dwm/dwm/config.h $HOME/suckless/dwm/config.h
-#fi
-#if [ ! -L ./st/config.h ]; then
-#	[ -f ./st/config.h ] && rm ./st/config.h
-#	ln -s $HOME/dots/conf/dwm/st/config.h $HOME/suckless/st/config.h
-#fi
-#
-#echo "[desktop.sh] Make the suckless..."
-#cd $HOME/suckless/dwm
-#sudo make install clean
-#cd $HOME/suckless/dmenu
-#sudo make install clean
-#cd $HOME/suckless/st
-#sudo make install clean
+echo "[desktop.sh] Symlink the sxwm config..."
+mkdir -p ~/.config
+ln -s ./dots/conf/sxwm/sxwmrc $HOME/.config/sxwmrc
+
+echo "[desktop.sh] Symlink the picom config..."
+ln -s ./dots/conf/sxwm/picom.conf $HOME/.config/sxwmrc
 
 echo "[desktop.sh] Symlink the .xinitrc & .Xresources..."
-[ -f $HOME/.xinitrc ] && rm $HOME/.xinitrc
-ln -s $HOME/dots/conf/dwm/xinitrc $HOME/.xinitrc
-[ -f $HOME/.Xresources ] && rm $HOME/.Xresources
+ln -s ./sxwm/xinitrc $HOME/.xinitrc
 sudo echo 'Xft.dpi: 180
 Xft.antialias: true' | sudo tee $HOME/.Xresources
 
