2bb56b5557f521b83231e9997f179ab360a23d34
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-23 20:53:51 +0300

    Auto-sync Fri May 23 08:53:51 PM MSK 2025

--- desktop.sh
+++ desktop.sh
@@ -6,7 +6,7 @@ echo $(dirname $(realpath $0))
 
 echo "[desktop.sh] Symlink the picom config..."
 mkdir -p ~/.config/picom
-rm $HOME/.config/picom
+rm $HOME/.config/picom/picom.conf
 ln -s $(pwd)/dwm/picom.conf $HOME/.config/picom/picom.conf
 
 echo "[desktop.sh] Symlink the .xinitrc & .Xresources..."
