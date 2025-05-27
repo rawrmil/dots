1915f0c14f24fe829eee640fe9237ba7cb74f434
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-27 12:07:07 +0300

    Auto-sync Tue May 27 12:07:07 PM MSK 2025

--- dwm/dwm-loop.sh
+++ dwm/dwm-loop.sh
@@ -3,8 +3,8 @@ set -x
 [ -z "$DISPLAY" ] && export DISPLAY=:0
 [ -z "$XDG_RUNTIME_DIR" ] && export XDG_RUNTIME_DIR=/tmp
 while true; do
-	xrdb -merge ~/.Xresources
-	setxkbmap -layout us,ru -option grp:alt_shift_toggle
-	picom -b --config ~/dots/conf/dwm/picom.conf
-	dwm
+  xrdb -merge ~/.Xresources
+  setxkbmap -layout us,ru -option grp:alt_shift_toggle
+  picom -b
+  dwm
 done
