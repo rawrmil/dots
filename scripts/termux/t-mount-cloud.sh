fb61ccfb699cb2ea3ad44f0045300b8c9661b9f1
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-29 10:53:44 +0300

    Auto-sync Thu May 29 10:53:44 MSK 2025

--- scripts/termux/t-mount-cloud.sh
+++ scripts/termux/t-mount-cloud.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 sudo nsenter -t 1 -m -- \
-sudo rclone mount nextcloud: /storage/emulated/0/mntnext --gid 9997 --dir-perms 0771 --file-perms 0660 --umask=0 --allow-other --vfs-cache-mode full --vfs-cache-max-size 10G --allow-non-empty
+sudo rclone mount nextcloud: /storage/emulated/0/Cloud \
+	--gid 9997 \
+	--dir-perms 0771 \
+	--file-perms 0660 \
+	--umask=0 \
+	--allow-other \
+	--vfs-cache-mode full \
+	--vfs-cache-max-size 10G \
+	--allow-non-empty \
+	--vfs-fast-fingerprint \
+	--dir-cache-time 1000h \
+    --vfs-cache-poll-interval 10s
+
