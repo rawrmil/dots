fbc9f7515e36605f61fdef15fbb2c67bb9b8a4af
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-05-30 18:55:18 +0300

    Auto-sync Fri May 30 06:55:18 PM MSK 2025

--- dwm/dwm-bar.sh
+++ dwm/dwm-bar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 xsetroot -name "Wait..."
 while true; do
-	BAT0=$(cat /sys/class/power_supply/BAT0/capacity >/dev/null)
+	BAT0=$(cat /sys/class/power_supply/BAT0/capacity 2>/dev/null)
 	[ -z $BAT0 ] && BATTERY="INF" || BATTERY="$BAT0%"
 	DATETIME=$(date '+%d.%m %H:%M')
 	xsetroot -name "BAT=$BATTERY | $DATETIME"
