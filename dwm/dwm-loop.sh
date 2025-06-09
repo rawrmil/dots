5783714fdb9a3a0f490caa606a5f3bdecaf53fa4
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-06-09 15:56:39 +0300

    Auto-sync Mon Jun  9 03:56:39 PM MSK 2025

--- dwm/dwm-loop.sh
+++ dwm/dwm-loop.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -x
 [ -z "$DISPLAY" ] && export DISPLAY=:0
-#[ -z "$XDG_RUNTIME_DIR" ] && export XDG_RUNTIME_DIR=/tmp
-sudo mkdir -p /run/user/$(id -u)
-sudo chown $USER:$USER /run/user/$(id -u)
-export XDG_RUNTIME_DIR=/run/user/$(id -u)
+[ -z "$XDG_RUNTIME_DIR" ] && export XDG_RUNTIME_DIR=/tmp
 while true; do
 	xrdb -merge ~/.Xresources
 	setxkbmap -layout us,ru -option grp:alt_shift_toggle
