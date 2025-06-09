237ae00851666c8e4e5a62cc51e5c0ecc63c0af3
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-06-09 12:25:23 +0300

    Auto-sync Mon Jun  9 12:25:23 PM MSK 2025

--- dwm/dwm-loop.sh
+++ dwm/dwm-loop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 [ -z "$DISPLAY" ] && export DISPLAY=:0
-[ -z "$XDG_RUNTIME_DIR" ] && export XDG_RUNTIME_DIR=/tmp
+#[ -z "$XDG_RUNTIME_DIR" ] && export XDG_RUNTIME_DIR=/tmp
 sudo mkdir -p /run/user/$(id -u)
 sudo chown $USER:$USER /run/user/$(id -u)
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
