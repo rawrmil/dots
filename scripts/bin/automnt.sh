ff3c5af21ce3faa522a0e881157b95fee06c9a36
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-06-21 13:22:59 +0300

    Auto-sync Sat Jun 21 01:22:59 PM MSK 2025

---
+++ scripts/bin/automnt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# automnt.sh root@192.168.0.122:8022 ~/POCOX3
+
+IFS=@/: read -r ssh_user ssh_ip ssh_port src_dir <<EOF
+${string:-$1}
+EOF
+dst_dir=$2
+
+echo $ssh_user $ssh_ip $ssh_port $src_dir $dst_dir
+mkdir -p $dst_dir
+sshfs -d -o uid=$(id -u $USER),gid=$(id -g $USER),allow_other \
+	-p $ssh_port $ssh_user@$ssh_ip:$src_dir $dst_dir
